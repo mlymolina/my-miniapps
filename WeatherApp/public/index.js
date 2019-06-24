64c262e2d6587669298bf661ce08fa9f0e2ddb04
Emely <mly.molina.minaya@gmail.com>
2019-06-24 11:26:24 -0700

    update in fetching to API

--- WeatherApp/public/index.js
+++ WeatherApp/public/index.js
@@ -4,13 +4,16 @@ searchElement.addEventListener('change', (event) => {
   fetch('/weather', {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json'
+      'Content-Type': 'application/json'
     },
     body: JSON.stringify({
       location: event.target.value
     })
-  }).then( res => res.json()).then(data => {
-    console.log(data)
+  }).then(res => res.json()).then(data => {
+    setWeather(data)
   })
 })
+
+let setWeather = (data) => {
+  console.log(data)
+}
--- WeatherApp/server.js
+++ WeatherApp/server.js
@@ -4,6 +4,7 @@ if (process.env.NODE_ENV !== 'production') {
 const W_API_KEY = process.env.API_KEY
 const bodyParser = require('body-parser')
 const express    = require('express')
+const axios      = require('axios')
 const app        = express()
 
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -14,8 +15,13 @@ app.use(express.json())
 app.use(express.static('public'))
 
 app.post('/weather', (req, res) => {
-  console.log('this is the body')
-  console.log(req.body)
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${req.body.location}&appid=${W_API_KEY}`
+  axios({
+    url: url,
+    responseType: 'json'
+  }).then(data => res.json(data.data)).catch((error) => {
+    console.log('Invalid City')
+  })
 })
 
 app.listen(PORT, () => {
